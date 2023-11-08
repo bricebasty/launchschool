cd0aef3a6f6a3d4160266da11ed3f16bbcb72ad6
celgost <brice.basty1@gmail.com>
2023-11-08 09:30:29 +0100

    typoglycemia refactor

--- codewars/typoglycemia.rb
+++ codewars/typoglycemia.rb
@@ -107,62 +107,39 @@ join this array
 
 def scramble_words(words)
   words.split.map do |word|
-    first_and_last_chars = word.scan(/(?<!\S)([-',\.]+)|([-',\.]+)(?!\S)/).flatten
-    first_and_last_letters = word.scan(/(?<!\S)(?:[-',\.]*)([a-z])|([a-z])(?:[-',\.]*)(?!\S)/i).flatten.compact
-    chars_in_between = word.scan(/(?<!\S)(?:[-',\.]*)(?:[a-z])(.+)(?:[a-z])(?:[-',\.]*)(?!\S)/i).join.chars
+    letters = word.scan(/[a-z]/)
 
-    positions_of_special_chars = chars_in_between.each_with_object({}).with_index do |(c, h), i|
-      if c.match?(/[-'\,\.]/)
-        h[c] = i
-        chars_in_between.delete_at(i)
-      end
-    end
-    chars_in_between.sort!
-    positions_of_special_chars.each { |k, v| chars_in_between.insert(v, k) }
-    [first_and_last_chars[0], first_and_last_letters[0], chars_in_between, first_and_last_letters[1], first_and_last_chars[1]].join
+    sorted_letters = letters[0..0] + letters[1..-2].sort + letters[-1..-1]
+    word.chars.map { |char| char.match?(/[a-z]/) ? sorted_letters.shift : char }.join
   end.join(" ")
 end
 
-def scramble_word(word)
+puts 'Test result is ' + (scramble_words('professionals') == 'paefilnoorsss').to_s.upcase
+p scramble_words('professionals') # 'paefilnoorsss', 'The first and last letters of a word should reamin in place with the inner letters sorted'
 
-end
+puts 'Test result is ' + (scramble_words('i') == 'i').to_s.upcase
+p scramble_words('i') # 'i', 'Must handle single charater words'
 
-# puts 'Test result is ' + (scramble_words('professionals') == 'paefilnoorsss').to_s.upcase
-# p scramble_words('professionals') # 'paefilnoorsss', 'The first and last letters of a word should reamin in place with the inner letters sorted'
+puts 'Test result is ' + (scramble_words('') == '').to_s.upcase
+p scramble_words('') # '', 'Must handle empty strings'
 
-# puts 'Test result is ' + (scramble_words('i') == 'i').to_s.upcase
-# p scramble_words('i') # 'i', 'Must handle single charater words'
+puts 'Test result is ' + (scramble_words('me') == 'me').to_s.upcase
+p scramble_words('me') # 'me', 'Must handle 2 charater words'
 
-# puts 'Test result is ' + (scramble_words('') == '').to_s.upcase
-# p scramble_words('') # '', 'Must handle empty strings'
+puts 'Test result is ' + (scramble_words('you') == 'you').to_s.upcase
+p scramble_words('you') # 'you', 'Must handle 3 charater words'
 
-# puts 'Test result is ' + (scramble_words('me') == 'me').to_s.upcase
-# p scramble_words('me') # 'me', 'Must handle 2 charater words'
+puts 'Test result is ' + (scramble_words('card-carrying') == 'caac-dinrrryg').to_s.upcase
+p scramble_words('card-carrying') # 'caac-dinrrryg', 'Only spaces separate words and punctuation should remain at the same place as it started'
 
-# puts 'Test result is ' + (scramble_words('you') == 'you').to_s.upcase
-# p scramble_words('you') # 'you', 'Must handle 3 charater words'
+puts 'Test result is ' + (scramble_words("shan't") == "sahn't").to_s.upcase
+p scramble_words("shan't") # "sahn't", 'Punctuation should remain at the same place as it started'
 
-# puts 'Test result is ' + (scramble_words('card-carrying') == 'caac-dinrrryg').to_s.upcase
-# p scramble_words('card-carrying') # 'caac-dinrrryg', 'Only spaces separate words and punctuation should remain at the same place as it started'
+puts 'Test result is ' + (scramble_words('-dcba') == '-dbca').to_s.upcase
+p scramble_words('-dcba') # '-dbca', 'Must handle special character at the start'
 
-# puts 'Test result is ' + (scramble_words("shan't") == "sahn't").to_s.upcase
-# p scramble_words("shan't") # "sahn't", 'Punctuation should remain at the same place as it started'
+puts 'Test result is ' + (scramble_words('dcba.') == 'dbca.').to_s.upcase
+p scramble_words('dcba.') # 'dbca.', 'Must handle special character at the end'
 
-# puts 'Test result is ' + (scramble_words('-dcba') == '-dbca').to_s.upcase
-# p scramble_words('-dcba') # '-dbca', 'Must handle special character at the start'
-
-# puts 'Test result is ' + (scramble_words('dcba.') == 'dbca.').to_s.upcase
-# p scramble_words('dcba.') # 'dbca.', 'Must handle special character at the end'
-
-# puts 'Test result is ' + (scramble_words("you've gotta dance like there's nobody watching, love like you'll never be hurt, sing like there's nobody listening, and live like it's heaven on earth.") == "you've gotta dacne like teehr's nbdooy wachintg, love like ylo'ul neevr be hrut, sing like teehr's nbdooy leiinnstg, and live like it's haeevn on earth.").to_s.upcase
-# p scramble_words("you've gotta dance like there's nobody watching, love like you'll never be hurt, sing like there's nobody listening, and live like it's heaven on earth.") # "you've gotta dacne like teehr's nbdooy wachintg, love like ylo'ul neevr be hrut, sing like teehr's nbdooy leiinnstg, and live like it's haeevn on earth.", 'Must handle a full sentence'
-
-
-# Create a test sentence and a test word
-test_sentence = "The quick brown fox jumps over the lazy dog."
-require 'benchmark'
-# Benchmark
-Benchmark.bm do |x|
-  x.report("scramble_words: ") { 1000.times { scramble_words(test_sentence) } }
-  x.report("scramble_word: ")  { 1000.times { scramble_words_2(test_sentence) } }
-end
+puts 'Test result is ' + (scramble_words("you've gotta dance like there's nobody watching, love like you'll never be hurt, sing like there's nobody listening, and live like it's heaven on earth.") == "you've gotta dacne like teehr's nbdooy wachintg, love like ylo'ul neevr be hrut, sing like teehr's nbdooy leiinnstg, and live like it's haeevn on earth.").to_s.upcase
+p scramble_words("you've gotta dance like there's nobody watching, love like you'll never be hurt, sing like there's nobody listening, and live like it's heaven on earth.") # "you've gotta dacne like teehr's nbdooy wachintg, love like ylo'ul neevr be hrut, sing like teehr's nbdooy leiinnstg, and live like it's haeevn on earth.", 'Must handle a full sentence'
