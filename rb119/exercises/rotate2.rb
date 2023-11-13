40c9b6e4845984f67ba4e9213982b9b71052c50d
celgost <brice.basty1@gmail.com>
2023-11-13 21:02:23 +0100

    rotate problem

--- rb119/exercises/rotate2.rb
+++ rb119/exercises/rotate2.rb
@@ -1,41 +1,59 @@
+=begin
+PROBLEM
+----------------
+INPUT: 2 integers
+OUTPUT: 1 integer
+
+RULES:
+- Rotate the last "second integer" digits on the right in the first integer
+- Rotation is from the first digits in the last "second integer digits"
+
+EXAMPLES
+----------------
+INPUT: 735291, 2
+=> [7, 3, 5, 2, 9, 1]
+=> [7, 3, 5, 2, 9, 1, 9]
+=> [7, 3, 5, 2, 1, 9]
+OUTPUT: 735219
+
+INPUT: 735291, 3
+=>
+=>
+OUTPUT: 735912
+
+INPUT: 735291,4
+=>
+=>
+OUTPUT: 732915
+
+DATA STRUCTURES
+----------------
+INPUT: Integer
+=> Arrays
+OUTPUT: Integer
+
+ALGORITHM
+----------------
+
+NOTES:
+
+HIGH-LEVEL:
+Extract the digits of the integer into an array of integers
+In this array we would push the element at negative index from the end
+Remove the element at negative index + 1
+Join the array
+
+LOW-LEVEL:
+
+=end
+
 def rotate_rightmost_digits(integer, rotation)
-  return integer if rotation == 1
-  array = integer.digits.reverse
-  return (array[1..-1] + [array[-rotation]]).join.to_i if rotation == integer.to_s.size
-  (array[0..-rotation - 1] + array[-rotation + 1..-1] + [array[-rotation]]).join.to_i
+  array_of_integers = integer.digits.reverse
+  array_of_integers << array_of_integers[-rotation]
+  array_of_integers.delete_at(-(rotation + 1))
+  array_of_integers.join.to_i
 end
 
-=begin
-EXAMPLES
-
-BEGIN: 735291, 2
-=> "7", "3", "5", "2", "9", "1"
-=> [0..-3] + [-1..-1] + [-2]
-END: 735219
-
-BEGIN: 735291, 3
-=> "7", "3", "5", "2", "9", "1"
-=> [0..-4] + [4..-1] + [-3]
-=> [0..-4] + [-2..-1] + [-3]
-END: 735912
-
-BEGIN: 735291, 4
-=> "7", "3", "5", "2", "9", "1"
-=> [0..1] + [-3..-1] + [-4]
-END: 732915
-
-BEGIN: 735291, 5
-=> "7", "3", "5", "2", "9", "1"
-=> [0..0] + [-4..-1] + [-5]
-END: 752913
-
-BEGIN: 735291, 6
-=> "7", "3", "5", "2", "9", "1"
-=> [1..-1] + [-6]
-END: 352917
-
-DATA STRUCTURE
-=end
 p rotate_rightmost_digits(735291, 1)  == 735291
 p rotate_rightmost_digits(735291, 2)  == 735219
 p rotate_rightmost_digits(735291, 3)  == 735912
