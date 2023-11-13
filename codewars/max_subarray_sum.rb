a48a4ccda9f234b2bd3d89397ec4de1be0131315
celgost <brice.basty1@gmail.com>
2023-11-13 21:02:17 +0100

    new version

--- codewars/max_subarray_sum.rb
+++ codewars/max_subarray_sum.rb
@@ -20,16 +20,38 @@ RULES:
 
 EXAMPLES
 ----------------
-INPUT: [-2, 1, -3, 4, -1, 2, 1, -5, 4]
-=> -2 -> non
-=> 1 -> oui -> 1 - 3 -> non
-=> -3 -> non
-=> 4 -> oui -> 4 - 1 -> 3 -> oui -> 3 + 2 -> 5 -> oui -> 5 + 1 -> 6 -> oui -> 6 - 5 -> 1 -> oui -> 1 + 4 -> 5 -> oui
+INPUT: [4, -1, 2, 1, -5, 4]
+
+=> [4] index 0 1
+=> [4, -1] index 0 2
+=> [4, -1, 2] index 0 3
+=> [4, -1, 2, 1] index 0 4
+=> [4, -1, 2, 1, -5] index 0 5
+=> [4, -1, 2, 1, -5, 4] index 0 6
+
+=> [-1] 1 1
+=> [-1, 2] 1 2
+=> [-1, 2, 1] 1 3
+=> [-1, 2, 1, -5] 1 4
+=> [-1, 2, 1, -5, 4] 1 5
+
+=> [2] 2 1
+=> [2, 1] 2 2
+=> [2, 1, -5] 2 3
+=> [2, 1, -5, 4] 2 4
+
+=> [1] 3 1
+=> [1, -5] 3 2
+=> [1, -5, 4] 3 3
+
+=> [-5] 4 1
+=> [-5, 4] 4 2
+
+=> [4]
 OUTPUT: 6
 
 INPUT: [-10, 20, -21, 30]
-=> -10 non
-=> 20 - 21 -> -1 + 30 = 40
+=>
 OUTPUT:
 
 DATA STRUCTURES
@@ -42,73 +64,39 @@ NOTES:
 
 ALGORITHM
 ----------------
+HIGH-LEVEL:
+Extract all the consecutive subarrays possible
+Remove duplicates
+Sum each of these arrays
+Take the max number in the resulting array
 
-Iterate through arr
-  If the element in the array is negative
-    Go to the next iteration
-  Else
-    Take the current element and add it to the next element
-    Push this result to another array
-    If this result added to the next element is positive
-      Add this new result to the array
-        If this new result added to the next element is positive
-          Add this new result to the array
-    Else
-      Go to next iteration
+LOW-LEVEL:
+Initialize an empty array `all_consecutive_numbers`
+For each index in the original array with its index called start_index
+  Create a max_size which will be the original array size minus the start_index
+  Each From 1 to max_size parameter is wanted_size
+    next if the all_consecutive_numbers includes value of original array at index start_index for size wanted_size
+    push to all_consecutive_numbers the array at index start_index for size wanted_size
 
-SET sum_array = []
-
-iterator = 0
-
-WHILE iterator <= length of array
-  IF current element < 0
-    NEXT
-  ELSE
-    SET current elemenet = element in arr at index iterator
-    SET result = current element
-    SET next element = array at index
-    SET sub_array = array from current element to last element
-    SET iterator2 = 0
-    WHILE result > 0 and iterator2 <= length of sub_array
-      result = current element + next element
-      add result to sum_array
-      iterator2 += 1
-
-  iterator += 1
-
-TAKE THE MAX FROM sum_array
-
+map all arrays in all_consecutives_numbers to their sum, and take the max of it
 =end
-def max_sequence(arr)
-  return 0 if arr.all?(&:negative?) || arr.empty?
-  return arr[0] if arr.size == 1
-  sum_array = []
-  iterator = 0
-  while iterator <= arr.size - 1
-    current_element = arr[iterator]
-    iterator += 1 if current_element.negative?
-    next if current_element.negative?
 
-    result = current_element
-    next_element = arr[iterator + 1]
-    sub_array = arr[iterator..-1]
-    iterator2 = 0
+def max_sequence(array)
+  return 0 if array.nil? || array.all?(&:negative?)
 
-    while !next_element.nil? && result > 0
-      sum_array << result if result.positive?
-
-      result += next_element
-      iterator2 += 1
-      next_element = sub_array[iterator2 + 1]
+  all_consecutive_numbers = []
+  array.each_index do |start_index|
+    max_size = array.size - start_index
+    (1..max_size).each do |wanted_size|
+      next if all_consecutive_numbers.include?(array[start_index, wanted_size])
+      all_consecutive_numbers << array[start_index, wanted_size]
     end
-
-  iterator += 1
   end
-  sum_array.max
+  all_consecutive_numbers.map(&:sum).max
 end
 
-# puts 'Test result is ' + (max_sequence([]) == 0).to_s.upcase
-# p max_sequence([]) # 0
+puts 'Test result is ' + (max_sequence([]) == 0).to_s.upcase
+p max_sequence([]) # 0
 
 puts 'Test result is ' + (max_sequence([4, -1, 2, 1, -5, 4]) == 6).to_s.upcase
 p max_sequence([4, -1, 2, 1, -5, 4]) # 6
