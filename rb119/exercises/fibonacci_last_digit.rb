1f3a495c9a18c0d41bb6597bd00ddbfb7da63ec1
celgost <brice.basty1@gmail.com>
2023-11-21 20:07:05 +0100

    last digit kata

--- rb119/exercises/fibonacci_last_digit.rb
+++ rb119/exercises/fibonacci_last_digit.rb
@@ -6,11 +6,47 @@
 # In this exercise, you are going to compute a method that returns the last
 # digit of the nth Fibonacci number.
 
-# Examples:
+=begin
+09:56
+PROBLEM
+---
+INPUT: Integer
+OUTPUT: Last digit of the integer'th fibonacci number
+RULES:
 
-fibonacci_last(15)        # -> 0  (the 15th Fibonacci number is 610)
-fibonacci_last(20)        # -> 5 (the 20th Fibonacci number is 6765)
-fibonacci_last(100)       # -> 5 (the 100th Fibonacci number is 354224848179261915075)
-fibonacci_last(100_001)   # -> 1 (this is a 20899 digit number)
-fibonacci_last(1_000_007) # -> 3 (this is a 208989 digit number)
-fibonacci_last(123456789) # -> 4
+EXAMPLES
+---
+INPUT:
+=>
+OUTPUT:
+
+DATA STRUCTURES
+---
+INPUT:
+=>
+OUTPUT:
+
+ALGO
+---
+
+WHAT:
+
+HOW:
+
+=end
+
+def fibonacci_last(nth)
+  first, last = [1, 1]
+  3.upto(nth) do
+    first, last = [last, first + last]
+  end
+
+  last.to_s[-1]
+end
+
+# p fibonacci_last(15)        # -> 0  (the 15th Fibonacci number is 610)
+# p fibonacci_last(20)        # -> 5 (the 20th Fibonacci number is 6765)
+# p fibonacci_last(100)       # -> 5 (the 100th Fibonacci number is 354224848179261915075)
+# p fibonacci_last(100_001)   # -> 1 (this is a 20899 digit number)
+# p fibonacci_last(1_000_007) # -> 3 (this is a 208989 digit number)
+p fibonacci_last(123456789) # -> 4
