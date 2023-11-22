aba21a0bf7df04022a0019eece4ceea2c92f781f
celgost <brice.basty1@gmail.com>
2023-11-22 08:28:31 +0000

    Nov 22, 2023, 9:28 AM

---
+++ rb119/exercises/matchingparentheses.rb
@@ -0,0 +1,16 @@
+# Write a method that takes a string as an argument, and returns true if all
+# parentheses in the string are properly balanced, false otherwise.
+# To be properly balanced, parentheses must occur in matching '(' and ')' pairs.
+# end
+
+# Note that balanced pairs must each start with a (, not a ).
+
+balanced?('What (is) this?') == true
+balanced?('What is) this?') == false
+balanced?('What (is this?') == false
+balanced?('((What) (is this))?') == true
+balanced?('((What)) (is this))?') == false
+balanced?('Hey!') == true
+balanced?(')Hey!(') == false
+balanced?('What ((is))) up(') == false
+balanced?('What ())(is() up') == false
