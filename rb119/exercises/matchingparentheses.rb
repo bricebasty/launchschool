16f071fdbfaab8360f8ed866547da4802896d5b5
celgost <brice.basty1@gmail.com>
2023-11-22 09:02:23 +0000

    Nov 22, 2023, 10:02 AM

--- rb119/exercises/matchingparentheses.rb
+++ rb119/exercises/matchingparentheses.rb
@@ -91,6 +91,17 @@ HOW ?
 
 #   parentheses.all? { |element| element.size.even? }
 # end
+#
+def balanced?(string)
+parens = 0
+string.each_char do |char|
+  parens += 1 if char.match?(/[\(\)'"\[\]\{\}]/)
+  parens -= 1 if char == ')'
+  break if parens < 0
+end
+
+parens.zero?
+end
 
 p balanced?('What (is) this?') == true
 p balanced?('What is) this?') == false
