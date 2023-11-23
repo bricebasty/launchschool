8d2a35081e56c6c3a1dbe4d245fca58e397671e5
celgost <brice.basty1@gmail.com>
2023-11-23 08:42:46 +0000

    Nov 23, 2023, 9:42 AM

---
+++ rb119/exercises/trianglesides.rb
@@ -0,0 +1,19 @@
+# A triangle is classified as follows:
+
+# equilateral All 3 sides are of equal length
+# isosceles 2 sides are of equal length, while the 3rd is different
+# scalene All 3 sides are of different length
+# To be a valid triangle, the sum of the lengths of the two shortest sides must be
+# greater than the length of the longest side, and all sides must have lengths
+# greater than 0: if either of these conditions is not satisfied, the triangle is invalid.
+
+# Write a method that takes the lengths of the 3 sides of a triangle as arguments,
+# and returns a symbol :equilateral, :isosceles, :scalene, or :invalid depending
+# on whether the triangle is equilateral, isosceles, scalene, or invalid.
+
+
+triangle(3, 3, 3) == :equilateral
+triangle(3, 3, 1.5) == :isosceles
+triangle(3, 4, 5) == :scalene
+triangle(0, 3, 3) == :invalid
+triangle(3, 1, 1) == :invalid
