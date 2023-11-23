56d368e126e89a68a41226d667680b83c1316da8
celgost <brice.basty1@gmail.com>
2023-11-23 09:06:38 +0000

    Nov 23, 2023, 10:06 AM

--- rb119/exercises/tri_angles.rb
+++ rb119/exercises/tri_angles.rb
@@ -33,9 +33,9 @@ RULES:
 
 EXAMPLES
 ---
-INPUT:
+INPUT: 60, 70, 50
 =>
-OUTPUT:
+OUTPUT: :acute
 
 DATA STRUCTURES
 ---
@@ -52,12 +52,12 @@ HOW
 =end
 
 def triangle(angle1, angle2, angle3)
-  all_angles = [angle1, angle2, angle3].sort
-  return :invalid if all_angles.any?(&:zero?) || all_angles[0..1].sum < all_angles[2]
+  all_angles = [angle1, angle2, angle3]
+  return :invalid if all_angles.any?(&:zero?) || all_angles.sum != 180
 end
 
-triangle(60, 70, 50) == :acute
-triangle(30, 90, 60) == :right
-triangle(120, 50, 10) == :obtuse
-triangle(0, 90, 90) == :invalid
-triangle(50, 50, 50) == :invalid
+p triangle(60, 70, 50) == :acute
+p triangle(30, 90, 60) == :right
+p triangle(120, 50, 10) == :obtuse
+p triangle(0, 90, 90) == :invalid
+p triangle(50, 50, 50) == :invalid
