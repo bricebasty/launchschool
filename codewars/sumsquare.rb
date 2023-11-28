fb024227c176c8e90ad2a5489277653cd9f6ba58
celgost <brice.basty1@gmail.com>
2023-11-28 09:18:18 +0000

    Nov 28, 2023, 10:18 AM

---
+++ codewars/sumsquare.rb
@@ -0,0 +1,11 @@
+Sum Square - Square Sum
+Write a method that computes the difference between the square of the sum of the first n positive integers and the sum of the squares of the first n positive integers.
+
+Examples:
+
+Copy Code
+sum_square_difference(3) == 22
+   # -> (1 + 2 + 3)**2 - (1**2 + 2**2 + 3**2)
+sum_square_difference(10) == 2640
+sum_square_difference(1) == 0
+sum_square_difference(100) == 25164150
