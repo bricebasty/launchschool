13aeb548b841e662d253b6192e683c80a06123f6
celgost <brice.basty1@gmail.com>
2023-11-29 09:13:19 +0000

    Nov 29, 2023, 10:13 AM

--- codewars/pile_of_cubes.rb
+++ codewars/pile_of_cubes.rb
@@ -6,8 +6,9 @@
 
 # You are given the total volume m of the building. Being given m can you find the number n of cubes you will have to build?
 
-# The parameter of the function findNb (find_nb, find-nb, findNb, ...) will be an integer m and you have to return the integer n such as
-
+# The parameter of the function findNb (find_nb, find-nb, findNb, ...) will be
+# an integer m and you have to return the integer n such as
+# n**3 + (n - 1)**3 + (n - 2)**3 ... + 1**3 = m
 # m if such a n exists or -1 if there is no such n.
 
 =begin
