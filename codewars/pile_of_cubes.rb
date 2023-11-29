f78d871eab2e9bab7634e4f0251bf701a7277567
celgost <brice.basty1@gmail.com>
2023-11-29 09:39:00 +0000

    Nov 29, 2023, 10:39 AM

--- codewars/pile_of_cubes.rb
+++ codewars/pile_of_cubes.rb
@@ -72,9 +72,6 @@ def find_nb(m)
   n
 end
 
-def find_nb(m)
-  Math.sqrt(2 * (Math.sqrt(1.0 * m))).to_i
-end
 
 puts 'Test result is ' + (find_nb(4183059834009) == 2022).to_s.upcase
 p find_nb(4183059834009) # 2022
